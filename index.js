71dfbf7bdbd2e6af8563d733bffc9f3dad4fe23c
Chase <chasefreeman13@gmail.com>
2016-11-30 13:05:17 -0500

    express routes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Get Request from Homepage');
 });
+
+app.post('/', function(req, res){
+  res.send('Post Request to Homepage')
+})
 
 app.listen(9999, function () {
   console.log('Example app listening on port 9999!');
